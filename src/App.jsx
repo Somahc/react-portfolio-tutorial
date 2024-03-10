fa916e189a3237697f3ca525e018928dae529fff
Soma Sekine <a8121151@aoyama.jp>
2024-03-10 00:58:54 -0500

    About Meセクション完成

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,80 @@ function App() {
           </div>
         </div>
       </section>
+
+      <section className="text-gray-700 border-t border-gray-200">
+        <div className="container px-5 py-24">
+          <div className="text-center mb-20">
+            <h1 className="text-2xl sm:text-3xl font-medium mb-2 text-gray-900">About Me</h1>
+            <p className="pb-10">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus magni perferendis dolorum natus doloribus repellat odio laborum itaque, labore reprehenderit!
+            </p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, molestiae animi vel voluptatem placeat quis corrupti dolorum aliquam dolorem, repudiandae dolores, sed odio labore. Distinctio, aperiam quam. Dolores nemo dicta modi ullam repudiandae quisquam, at aspernatur voluptas iste! Aspernatur soluta assumenda eaque id, dignissimos dolorum tenetur quo minus consectetur debitis laboriosam corrupti, eligendi tempore alias illo nam unde cupiditate dicta repellat optio pariatur. Cumque illo consequuntur voluptas harum beatae quam, magni accusantium! Recusandae magni ducimus velit perspiciatis pariatur nam, quas quam aliquam quos esse ipsa fugit fugiat id numquam exercitationem! Debitis facere dolorem tempora sint molestiae labore nisi voluptas ea.</p>
+          </div>
+          {/* カードのdivタグ */}
+          <div className="flex flex-wrap">
+            <div className="md:w-1/3 p-4">
+              <div className="bg-gray-100 rounded-lg p-8">
+                <div className="flex items-center mb-3">
+                  <div className="bg-green-500 text-white rounded-full">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.2426 16.3137L6 12.071L7.41421 10.6568L10.2426 13.4853L15.8995 7.8284L17.3137 9.24262L10.2426 16.3137Z" fill="currentColor" /><path fill-rule="evenodd" clip-rule="evenodd" d="M1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21Z" fill="currentColor" /></svg>
+                  </div>
+                  <h2 class="text-gray-900 text-lg font-medium ml-2">Web Developer</h2>
+                </div>
+                <div>
+                  <p className="text-left">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi quisquam illum totam iusto hic sit provident ut consectetur eum reprehenderit?
+                  </p>
+                  <a className="flex mt-3 text-green-500 items-center" href="">
+                    もっと見る
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z" fill="currentColor" /></svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="md:w-1/3 p-4">
+              <div className="bg-gray-100 rounded-lg p-8">
+                <div className="flex items-center mb-3">
+                  <div className="bg-green-500 text-white rounded-full">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.2426 16.3137L6 12.071L7.41421 10.6568L10.2426 13.4853L15.8995 7.8284L17.3137 9.24262L10.2426 16.3137Z" fill="currentColor" /><path fill-rule="evenodd" clip-rule="evenodd" d="M1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21Z" fill="currentColor" /></svg>
+                  </div>
+                  <h2 class="text-gray-900 text-lg font-medium ml-2">Web Developer</h2>
+                </div>
+                <div>
+                  <p className="text-left">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi quisquam illum totam iusto hic sit provident ut consectetur eum reprehenderit?
+                  </p>
+                  <a className="flex mt-3 text-green-500 items-center" href="">
+                    もっと見る
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z" fill="currentColor" /></svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="md:w-1/3 p-4">
+              <div className="bg-gray-100 rounded-lg p-8">
+                <div className="flex items-center mb-3">
+                  <div className="bg-green-500 text-white rounded-full">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.2426 16.3137L6 12.071L7.41421 10.6568L10.2426 13.4853L15.8995 7.8284L17.3137 9.24262L10.2426 16.3137Z" fill="currentColor" /><path fill-rule="evenodd" clip-rule="evenodd" d="M1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21Z" fill="currentColor" /></svg>
+                  </div>
+                  <h2 class="text-gray-900 text-lg font-medium ml-2">Web Developer</h2>
+                </div>
+                <div>
+                  <p className="text-left">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi quisquam illum totam iusto hic sit provident ut consectetur eum reprehenderit?
+                  </p>
+                  <a className="flex mt-3 text-green-500 items-center" href="">
+                    もっと見る
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z" fill="currentColor" /></svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      
     </>
   )
 }
